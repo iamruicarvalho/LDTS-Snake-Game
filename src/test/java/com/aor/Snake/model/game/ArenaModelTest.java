aea028cece1ae1ce016133e75be29e26d133a457
Guilherme Ribeiro <up202108731@up.pt>
2022-12-23 18:08:43 +0000

    Killed some Mutants in Arena Model

--- src/test/java/com/aor/Snake/model/game/ArenaModelTest.java
+++ src/test/java/com/aor/Snake/model/game/ArenaModelTest.java
@@ -2,7 +2,10 @@ package com.aor.Snake.model.game;
 
 import com.aor.Snake.Game;
 import com.aor.Snake.Controller.game.SnakeController;
+import com.aor.Snake.ScoreBoard.SingletonScoreboard;
 import com.aor.Snake.model.Position;
+import com.aor.Snake.model.game.Fruit.Banana;
+import com.aor.Snake.model.game.Fruit.Cherry;
 import com.aor.Snake.model.game.Fruit.FruitFactory;
 import com.aor.Snake.model.game.arena.Arena;
 import com.aor.Snake.model.game.Fruit.Apple;
@@ -23,13 +26,12 @@ public class ArenaModelTest {
     private Game game;
     private SnakeController snakeController;
     private Apple apple;
+    private Banana banana;
+    private Cherry cherry;
 
     @BeforeEach
     void setup(){
         arena = new Arena(29,29);
-
-        apple = (Apple) FruitFactory.createFruit("Apple", new Position(9, 10));
-        arena.setFruit(apple);
 
         List<SnakeBody> snake = new ArrayList<>();
         snake.add(new SnakeBody(10, 10));
@@ -45,14 +47,16 @@ public class ArenaModelTest {
         game = Mockito.mock(Game.class);
     }
 
+
     @Test
     void isAppleWorking(){
+        apple = (Apple) FruitFactory.createFruit("Apple", new Position(9, 10));
+        arena.setFruit(apple);
         assertEquals(true, arena.isFruit(arena.getFruit().getPosition()));
     }
 
     @Test
     void isEmptyWorking(){
-
         //Walls
         assertEquals(false, arena.isEmpty(arena.getWalls().get(0).getPosition()));
 
@@ -61,6 +65,42 @@ public class ArenaModelTest {
 
         //EmptySpace
         assertEquals(true, arena.isEmpty(new Position(15, 15)));
+    }
 
+    @Test
+    void scoreTest(){
+        assertEquals(0, arena.getScore());
+        arena.IncrementScore(2);
+        assertEquals(2, arena.getScore());
+
+        arena.MultiplyScore(2);
+        assertNotEquals(4, arena.getScore());
+        assertEquals(7, arena.getScore());
+
+        arena.MultiplyScore(3);
+        assertEquals(21, arena.getScore());
+    }
+
+    @Test
+    void moveFruitTest(){
+        apple = (Apple) FruitFactory.createFruit("Apple", new Position(9, 10));
+        arena.setFruit(apple);
+
+        assertEquals(0, arena.getScore());
+        arena.moveFruit();
+        assertEquals(1, arena.getScore());
+
+        banana = (Banana) FruitFactory.createFruit("Banana", new Position(20, 10));
+        arena.setFruit(banana);
+
+        assertEquals(1, arena.getScore());
+        arena.moveFruit();
+        assertEquals(3, arena.getScore());
+
+        cherry = (Cherry) FruitFactory.createFruit("Cherry", new Position(21, 10));
+        arena.setFruit(cherry);
+        arena.IncrementScore(3);
+        arena.moveFruit();
+        assertEquals(9, arena.getScore());
     }
 }
