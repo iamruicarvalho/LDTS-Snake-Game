ed5003b5b11197fd1cd02db063647bccebe70dcb
Guilherme Ribeiro <up202108731@up.pt>
2022-12-23 23:28:26 +0000

    Corrected the Tests due to the corrected Bug

--- src/test/java/com/aor/Snake/viewer/menu/ScoreBoardMenuViewerTest.java
+++ src/test/java/com/aor/Snake/viewer/menu/ScoreBoardMenuViewerTest.java
@@ -1,5 +1,6 @@
 package com.aor.Snake.viewer.menu;
 
+import com.aor.Snake.ScoreBoard.SingletonScoreboard;
 import com.aor.Snake.gui.LanternaGUI;
 import com.aor.Snake.model.Position;
 import com.aor.Snake.model.menu.ScoreBoardMenu;
@@ -16,11 +17,14 @@ public class ScoreBoardMenuViewerTest {
     private ScoreBoardMenu model;
     private LanternaGUI gui;
 
+    private SingletonScoreboard singletonScoreboard;
+
     @BeforeEach
     void setUp(){
         model = new ScoreBoardMenu();
         scoreBoardMenuViewer = new ScoreBoardMenuViewer(model);
         gui = Mockito.mock(LanternaGUI.class);
+        singletonScoreboard = new SingletonScoreboard();
     }
 
     @Test
@@ -30,9 +34,10 @@ public class ScoreBoardMenuViewerTest {
 
     @Test
     void drawElementsTest() throws IOException {
+        SingletonScoreboard instance = singletonScoreboard.getInstance();
         scoreBoardMenuViewer.drawElements(gui);
         Mockito.verify(gui, Mockito.times(1)).drawText(new Position(7, 10), "@ SCOREBOARD @", "#000CFF", "#000000");
-        Mockito.verify(gui, Mockito.times(1)).drawText(new Position(6, 12), "BEST SCORE: " + scoreBoardMenuViewer.getModel().getHighScore(), "#FFD700", "#000000");
+        Mockito.verify(gui, Mockito.times(1)).drawText(new Position(6, 12), "BEST SCORE: " + instance.getHighScore(), "#FFD700", "#000000");
         Mockito.verify(gui, Mockito.times(1)).drawText(new Position(6, 15), scoreBoardMenuViewer.getModel().getEntry(0), scoreBoardMenuViewer.getModel().isSelected(0) ? "#D97F02" : "#FFFFFF", "#000000");
         Mockito.verify(gui, Mockito.times(1)).drawText(new Position(6, 15), scoreBoardMenuViewer.getModel().getEntry(0), "#D97F02", "#000000");
         Mockito.verify(gui, Mockito.times(1)).drawText(new Position(6, 15), "BACK", "#D97F02", "#000000");
